24fa21c6afb458b7c6b805647aebcb81bebdf4cb
Carlos Garcia <carlosggz@gmail.com>
2020-07-01 22:58:50 +0200

    Added docker compose

--- api/ContactsBook.Api/Startup.cs
+++ api/ContactsBook.Api/Startup.cs
@@ -88,7 +88,7 @@ namespace ContactsBook.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("Docker"))
             {
                 app.UseDeveloperExceptionPage();
                 ApplyMigrations(app);
